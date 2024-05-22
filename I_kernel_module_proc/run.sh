8ca7bf397d39be94a28368c0c6da64f920bda7bb
rubychou0923 <40191221+rubychou0923@users.noreply.github.com>
2024-05-22 08:53:28 +0800

    Update run.sh

--- I_kernel_module_proc/run.sh
+++ I_kernel_module_proc/run.sh
@@ -1,1 +1,8 @@
-sudo rmmod hello.ko ; make ; sudo dmesg -C ; sudo insmod hello.ko ; sudo cat /proc/rf_status
+set -x
+sudo rmmod hello.ko
+make clean
+make 
+sudo dmesg -C
+sudo insmod hello.ko
+dmesg
+sudo cat /proc/rf_status
